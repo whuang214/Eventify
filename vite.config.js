83cedc79279f27c221fbb57030d2c74d3644ad24
whuang214 <whuang2146@gmail.com>
2024-06-20 13:12:58 -0400

    add million js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import MillionLint from '@million/lint';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+const _plugins = [react()];
+_plugins.unshift(MillionLint.vite())
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: _plugins
+});
